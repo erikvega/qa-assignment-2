e0c8c9d8ffffc279b3c5c6e89d3fd8ce5cf35c75
Erik <heyyyerik@gmail.com>
2021-03-04 16:26:05 -0600

    better tests and retirement polish api

--- source/test_retirement_api.py
+++ source/test_retirement_api.py
@@ -1,8 +1,9 @@
 import unittest
 from retirement_api import calculateSavingsPerYear, calulateYearsUntilGoal, calculateAgeWhenMet, printRetirementPlan, getAge, getGoal, getPercentSaved, getSalary
+from unittest.mock import patch
 
 class retirementCalculationTest(unittest.TestCase): 
-    def testSavingPerYear(self):
+    def testSavingsPerYear(self):
         salary = 65000
         percentSaved = 0.1
         savingPerYear = calculateSavingsPerYear(salary, percentSaved)
@@ -10,7 +11,7 @@ class retirementCalculationTest(unittest.TestCase):
 
     def testYearsUntilGoalMet(self):
         goal = 1500000
-        savingsPerYear = 65000
+        savingsPerYear = 8775
         yearsUntilGoal = calulateYearsUntilGoal(goal, savingsPerYear)
         self.assertEqual(yearsUntilGoal, 171)
 
@@ -25,27 +26,92 @@ class retirementPrintTest(unittest.TestCase):
     def testPrintRetirmentPlanOver100(self):
         ageWhenMet = 100
         result = printRetirementPlan(ageWhenMet) 
-        self.assertEqual(result, "Savings goal not met.")
+        self.assertEqual(result, "You would be 100 by the time you meet your savings goal. Savings goal not met.")
     
     def testPrintRetirmentPlanValid(self):
         ageWhenMet = 80 
         result = printRetirementPlan(ageWhenMet)
         self.assertEqual(result, "You will be 80 years old when you meet your goal.")
 
-class retirementUserInputTest(unittest.TestCase):
-    def test_bad_age(self):
+class retirementBadUserInputTest(unittest.TestCase):
+    stringInput = "hey"
+    zeroInput = 0
+    ageBoundaries = [zeroInput, 100]
+    salaryBoundaries = [zeroInput, 500001]
+    percentageBoundaries = [-1, 101]
+    noInput = " "
+
+    @patch('builtins.input', side_effect=stringInput)
+    def test_word_age(self, mock_input):
+        result = getAge()
+        self.assertEqual(result, -1)    
+    
+    @patch('builtins.input', side_effect=ageBoundaries[0])
+    def test_zero_age(self, mock_input):
         result = getAge()
         self.assertEqual(result, -1)
 
-    def test_bad_salary(self):
+    @patch('builtins.input', side_effect=ageBoundaries[1])
+    def test_100_age(self, mock_input):
+        result = getAge()
+        self.assertEqual(result, -1)
+    
+    @patch('builtins.input', side_effect=noInput)
+    def test_blank_age(self, mock_input):
+        result = getAge()
+        self.assertEqual(result, -1)
+
+    @patch('builtins.input', side_effect=stringInput)
+    def test_word_salary(self, mock_input):
         result = getSalary()
         self.assertEqual(result, -1)
 
-    def test_base_percentSaved(self):
+    @patch('builtins.input', side_effect=salaryBoundaries[0])
+    def test_zero_salary(self, mock_input):
+        result = getSalary()
+        self.assertEqual(result, -1)
+
+    @patch('builtins.input', side_effect=salaryBoundaries[1])
+    def test_over_max_salary(self, mock_input):
+        result = getSalary()
+        self.assertEqual(result, -1)
+
+    @patch('builtins.input', side_effect=noInput)
+    def test_blank_salary(self, mock_input):
+        result = getSalary()
+        self.assertEqual(result, -1)
+
+    @patch('builtins.input', side_effect=stringInput)
+    def test_word_percentSaved(self, mock_input):
         result = getPercentSaved()
         self.assertEqual(result, -1)
 
-    def test_bad_goal(self):
+    @patch('builtins.input', side_effect=-percentageBoundaries[0])
+    def test_negative_percentSaved(self, mock_input):
+        result = getPercentSaved()
+        self.assertEqual(result, -1)
+
+    @patch('builtins.input', side_effect=-percentageBoundaries[1])
+    def test_over_max_percentSaved(self, mock_input):
+        result = getPercentSaved()
+        self.assertEqual(result, -1)
+
+    @patch('builtins.input', side_effect=noInput)
+    def test_blank_percentSaved(self, mock_input):
+        result = getPercentSaved()
+        self.assertEqual(result, -1)
+
+    @patch('builtins.input', side_effect=stringInput)
+    def test_word_goal(self, mock_input):
         result = getGoal()
         self.assertEqual(result, -1)
 
+    @patch('builtins.input', side_effect=zeroInput)
+    def test_zero_goal(self, mock_input):
+        result = getGoal()
+        self.assertEqual(result, -1)
+
+    @patch('builtins.input', side_effect=noInput)
+    def test_blank_goal(self, mock_input):
+        result = getGoal()
+        self.assertEqual(result, -1)
